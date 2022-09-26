0063ebe761d19944b5e740f489665522f97c7fbb
rvt <alutoa2411@ieselcaminas.org>
2022-09-26 15:35:30 +0200

    forth commit

--- src/Example18.java
+++ src/Example18.java
@@ -3,30 +3,33 @@ import java.util.Scanner;
 public class Example18 {
     public static void main(String[] args){
 
-        double price,hours,gross,net,taxes;
+        double price,hour,gross,net,taxes;
         Scanner inputValue;
-        System.out.println("Enter price per hour:");
-
         inputValue = new Scanner(System.in);
+
+        System.out.println("Enter price per hour:");
+        hour = inputValue.nextDouble();
+        System.out.println("Enter number of hours:");
         price = inputValue.nextDouble();
 
-        System.out.println("Enter number of hours:");
-
-        hours = inputValue.nextDouble();
-
-        if (hours <= 35){
-            gross = price * hours;
+        if (hour <= 35) {
+            gross = price * hour;
         } else {
-            gross = 35 * price + (hours - 35) * price * 1.5;
+            gross = 35 * price + (hour - 35) * price * 1.5;
         }
         if (gross > 900) {
             taxes = (gross - 900) * 0.45 + 400 * 0.25;
+            net = gross - taxes;
         } else if (gross > 500) {
             taxes = (gross - 500) * 0.25;
+            net = gross+ taxes;
         } else {
+           taxes = 0;
             net = gross;
         }
-        System.out.println("Salary: " +gross);
+        System.out.println("Gross: " +gross);
+        System.out.println("Net: " + net);
+        System.out.println("Taxes: " + taxes);
     }
 
 }
